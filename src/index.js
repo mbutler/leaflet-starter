cd7b4659960c8a17c8968c63ec221a353321398c
Matthew Butler <matthewtbutler@gmail.com>
2019-04-01 15:07:25 -0500

    binding popups working

--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
 import L from 'leaflet'
 
 const map = L.map('map', {
-  center: [0, 0],
-	zoom: 3,
-	minZoom: 3,
-	maxZoom: 12
+    center: [0, 0],
+    zoom: 3,
+    minZoom: 3,
+    maxZoom: 12
 })
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-	attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 12
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 12
 }).addTo(map)
 
-let blueCircleMarkers = {
-  radius: 8,
-  fillColor: "#00AEEF",
-  color: "#3978BF",
-  weight: 1,
-  opacity: 0.5,
-  fillOpacity: 0.5
+let markercolor = {
+    radius: 8,
+    fillColor: "#00AEEF",
+    color: "#3978BF",
+    weight: 1,
+    opacity: 0.5,
+    fillOpacity: 0.5
+}
+
+let mainlayer = new L.geoJson(letters, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, markercolor)
+    }
+}).addTo(map)
+
+//let marker = L.circleMarker(latlng, blueCircleMarkers).addTo(map)
+
+function onEachFeature(feature, layer) {
+
+    let prop = feature.properties
+    let url
+    let letterBody
+
+    if (prop['URL'] !== "") {
+        url = prop['URL']
+        $.get(url, (res) => {
+            letterBody = $(res).find("body").html()
+            let popup = `<h3> ${prop['Sender']} to ${prop['Recipient']} on ${prop['time']} </h3><br> ${letterBody}`
+
+            feature.layer = layer
+            layer.bindPopup(popup, {
+                maxWidth: "auto"
+            })
+        })
+    } else {
+        url = "No digital transcription at this time"
+        let popup = `<h3> ${prop['Sender']} to ${prop['Recipient']} on ${prop['time']} </h3><br> ${url}`
+        feature.layer = layer
+        layer.bindPopup(popup, {
+            maxWidth: "auto"
+        })
+    }
 }
